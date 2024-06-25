d03a2706310e3e17602421a9b5573744487f017e
Christoffer Artmann <artgaard@gmail.com>
2024-06-25 20:23:05 +0200

    fix: Add a missing mock.

--- app/tests/routes/home-page.spec.tsx
+++ app/tests/routes/home-page.spec.tsx
@@ -6,11 +6,19 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom'
+import * as remix from '@remix-run/react'
 
 import HomePageRoute from '~/routes/_index'
 
 describe('HomePageRoute', () => {
   it('uses the price and number of reviews to calculate the gross & net revenue.', async () => {
+    const spy = jest.spyOn(remix, 'useLoaderData')
+
+    // @ts-ignore
+    spy.mockImplementation(() => ({
+      popularGames: []
+    }))
+
     render(
       <MemoryRouter>
         <HomePageRoute />
