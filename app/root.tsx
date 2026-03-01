508e4d27d50d77beccaec7a73bf2fca67f0e6356
Christoffer Artmann <artgaard@gmail.com>
2026-03-01 22:33:46 +0100

    Add a consent banner

--- app/root.tsx
+++ app/root.tsx
@@ -8,11 +8,11 @@ import {
   Scripts,
   ScrollRestoration
 } from '@remix-run/react'
-import { useEffect, useRef } from 'react'
-
 import tailwind from './tailwind.css'
 import styles from './global.css'
 import blogStyles from './blog.css'
+import cookieConsentStyles from 'vanilla-cookieconsent/dist/cookieconsent.css'
+import { CookieConsentBanner } from './components/cookie-consent'
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -29,7 +29,8 @@ export const links: LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap'
   },
-  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  { rel: 'stylesheet', href: cookieConsentStyles }
 ]
 
 export const meta: V2_MetaFunction = () => {
@@ -71,72 +72,9 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-        <GoogleScripts />
+        <CookieConsentBanner />
       </body>
     </html>
   )
 }
 
-interface Script {
-  async?: boolean
-  crossOrigin?: 'anonymous' | 'use-credentials'
-  html?: string
-  src?: string
-}
-
-function GoogleScripts() {
-  const addedScripts = useRef<Record<string, boolean>>({})
-
-  useEffect(() => {
-    const scripts: Script[] = [
-      {
-        async: true,
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-ZRVV1BYVCR'
-      },
-      {
-        html: `window.dataLayer = window.dataLayer
-        || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
-        gtag('config', 'G-6XN7FH6J2P');`
-      },
-      {
-        async: true,
-        crossOrigin: 'anonymous',
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7586045516328295'
-      }
-    ]
-
-    scripts.forEach((script) => {
-      if (!script.src && !script.html) {
-        throw new Error('Either src or html must be provided.')
-      }
-
-      const key = script.src || (script.html as string)
-
-      if (addedScripts.current[key]) {
-        return
-      }
-
-      const element = document.createElement('script')
-
-      element.async = script.async ?? false
-
-      if (script.crossOrigin) {
-        element.crossOrigin = script.crossOrigin
-      }
-
-      if (script.src) {
-        element.src = script.src
-      }
-
-      if (script.html) {
-        element.innerHTML = script.html
-      }
-
-      document.head.appendChild(element)
-
-      addedScripts.current[key] = true
-    })
-  }, [])
-
-  return null
-}
